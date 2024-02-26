3f40ef75d1b5997c1508b7e6461b1bd100256ef2
albim72 <marcin.albiniak72@gmail.com>
2024-02-26 10:27:41 +0100

    Update main.py

--- DZIEN_1/main.py
+++ DZIEN_1/main.py
@@ -1,1 +1,10 @@
+print("program testowy Python 3")
 
+liczba = [4,6,2,6,2,78,90,-11,103,57,12,34,-56,12,45,101,-3,0]
+
+parzyste = list(filter(lambda x:x%2==0,liczba))
+
+print(parzyste)
+
+cube = list(map(lambda x:x**3,liczba))
+print(cube)
