35a16c0c91f0bd9ef36e0fb1760849f4306a750e
albim72 <marcin.albiniak72@gmail.com>
2024-02-27 11:47:53 +0100

    Create indianie_pima.py

---
+++ DZIEN_2/indianie_pima.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+
+
+col_names = ["pregnant","glucose","bp","skin","insulin","bmi","pedigree","age","label"]
+pima = pd.read_csv('diabetes.csv',header=None,names=col_names)
+
+print(pima.head())
+
+feature_cols = ["pregnant","insulin","bmi","age","glucose","bp","pedigree"]
+X = pima[feature_cols]
+y = pima.label
+
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.25,random_state=43)
+
+clf = DecisionTreeClassifier(max_depth=22)
+clf = clf.fit(X_train,y_train)
+
+y_pred = clf.predict(X_test)
+
+print(f"dokładność dopasowania wyników: {metrics.accuracy_score(y_test,y_pred)}")
